98513015d5ca4e79fd59923a6c7173081bebdba2
kyrylo <kpogrebnyak.28@gmail.com>
2023-05-30 14:35:28 +0200

    updated docx2pdf file. now it convert docx template which is inside the docx folder to pdf. save new file to the pdfs folder

--- docx2pdf.py
+++ docx2pdf.py
@@ -1,25 +1,14 @@
-import os
-from docxtpl import DocxTemplate
 import subprocess
-from docx import Document
 
-def convert_docx_to_pdf(docx_file, pdf_file):
-    # Load the DOCX document
-    doc = Document(docx_file)
+def convert_docx_to_pdf(docx_file_path, pdf_file_path):
+    subprocess.run(['unoconv', '-f', 'pdf', '-o', pdf_file_path, docx_file_path])
+    print("Conversion successful!")
 
-    # Create a temporary file for the intermediate ODT format
-    odt_file = os.path.splitext(docx_file)[0] + '.odt'
-    doc.save(odt_file)
+# Specifing the input DOCX file path
+docx_path = "docx/template.docx"
 
-    # Use unoconv to convert ODT to PDF
-    subprocess.run(['unoconv', '-f', 'pdf', '-o', os.path.dirname(pdf_file), odt_file])
+# Specifing the output PDF file path and name
+pdf_path = "pdfs/template.pdf"
 
-    # Remove the intermediate ODT file
-    os.remove(odt_file)
-
-# Usage example
-docx_file = 'template.docx'  # Replace with the path to your DOCX file
-pdf_file = 'output.pdf'   # Replace with the desired output PDF path
-
-convert_docx_to_pdf(docx_file, pdf_file)
-print('Conversion complete. PDF file:', pdf_file)
+# Convert DOCX to PDF
+convert_docx_to_pdf(docx_path, pdf_path)
